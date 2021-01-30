227bd870758f1b8e9300ff48046ebf8a50db9e7f
sadikcoban <sadik.coban@agu.edu.tr>
2021-01-31 00:12:58 +0300

    redirecting to homepage after auth is implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 
 import HomePage from "./pages/homepage/homepage.component"
 import "./pages/homepage/homepage.styles.scss"
@@ -13,7 +13,6 @@ import { setCurrentUser } from "./redux/user/user.actions"
 
 
 class App extends React.Component {
-
 
   unsubscribeFromAuth = null;
 
@@ -46,15 +45,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/auth" component={AuthPage} />
+          <Route
+            exact
+            path="/auth"
+            render={() => this.props.currentUser ?
+              (<Redirect to="/" />)
+              : (<AuthPage />)
+            } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
